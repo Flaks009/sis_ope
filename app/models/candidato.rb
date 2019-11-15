53fceb495782bded64f4a29859c14d1738bc75e0
flaks009 <flaks009@gmail.com>
2019-11-15 18:40:13 -0300

    Fix Regex

--- app/models/candidato.rb
+++ app/models/candidato.rb
@@ -2,10 +2,10 @@ class Candidato < ApplicationRecord
     
     ## Validacoes
     validates :cpf, uniqueness: true
-    validates :nome, format: { with: /\A[a-z A-Z]+\z/, message: "Insira somente letras no campo nome" }
-    validates :logradouro, format: { with: /\A[a-z A-Z]+\z/, message: "Insira somente letras no campo logradouro" }
-    validates :bairro, format: { with: /\A[a-z A-Z]+\z/, message: "Insira somente letras no campo bairro" }
-    validates :cidade, format: { with: /\A[a-z A-Z]+\z/, message: "Insira somente letras no campo cidade" }    
+    validates :nome, format: { with: /\A[a-záàâãéèêíïóôõöúçñ ]+\z/i, message: "Insira somente letras no campo nome" }
+    validates :logradouro, format: { with: /\A[a-záàâãéèêíïóôõöúçñ ]+\z/i, message: "Insira somente letras no campo logradouro" }
+    validates :bairro, format: { with: /\A[a-záàâãéèêíïóôõöúçñ ]+\z/i, message: "Insira somente letras no campo bairro" }
+    validates :cidade, format: { with: /\A[a-záàâãéèêíïóôõöúçñ ]+\z/i, message: "Insira somente letras no campo cidade" }    
     validates :cep, numericality: { only_integer: true, message:"Campo CEP só aceita números" }, length: {is: 8, message: "CEP deve conter 8 digitos"}    
     validates :numero, numericality: { only_integer: true, message:"Campo número só aceita números" }
     
