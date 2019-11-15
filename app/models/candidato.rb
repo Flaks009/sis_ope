c9d0c5b1f10e2525f50a6b410a1c1f284fbac934
flaks009 <flaks009@gmail.com>
2019-11-15 13:05:00 -0300

    Candidatos: form fields validates

--- app/models/candidato.rb
+++ app/models/candidato.rb
@@ -1,5 +1,11 @@
 class Candidato < ApplicationRecord
     validates :cpf, uniqueness: true
+    validates :nome, format: { with: /\A[a-z A-Z]+\z/, message: "Insira somente letras no campo nome" }
+    validates :logradouro, format: { with: /\A[a-z A-Z]+\z/, message: "Insira somente letras no campo logradouro" }
+    validates :bairro, format: { with: /\A[a-z A-Z]+\z/, message: "Insira somente letras no campo bairro" }
+    validates :cidade, format: { with: /\A[a-z A-Z]+\z/, message: "Insira somente letras no campo cidade" }    
+    validates :cep, numericality: { only_integer: true, message:"Campo CEP só aceita números" }, length: {is: 8, message: "CEP deve conter 8 digitos"}    
+    validates :numero, numericality: { only_integer: true, message:"Campo número só aceita números" }
     belongs_to :user
     has_many :course, :class_name => "Course", :foreign_key => "cpf_candidato", :primary_key => "cpf"
     has_many :formation, :class_name => "Formation", :foreign_key => "cpf_candidato", :primary_key => "cpf"
