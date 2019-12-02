6abf060e4c3a97af9f2792a50f7ac6b9e9c92388
Eduardo Albuquerque <39992696+eajose@users.noreply.github.com>
2019-12-02 16:07:31 -0300

    Configuração do host para a função "Redefinir senha"

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'railsh.herokuapp.com'}
 
   ActionMailer::Base.smtp_settings = {
   :user_name => 'sis_ope',
